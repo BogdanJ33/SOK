21d210d0569b836ea4f3e12eb956eb0c683ba5b8
Bogdan <bogdanjanosevic@gmail.com>
2023-10-14 10:57:28 +0200

    #2 - Changed structure of my core

--- Core/Setup.py
+++ Core/Setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
+
 setup(
-    name="sok-core",
+    name="my-core",
     version="0.1",
     packages=find_packages(),
-    entry_points = {
-        'console_scripts':
-            ['core_main=core.core_main:main'],
-    },
-    zip_safe=True
+    # requiring Django later than 2.1
+    install_requires=['Django>=2.1'],
+    provides=['rs.uns.ftn.studentska.sluzba.services',
+              ],
+    # Installing package data related to packge
+    # https://docs.python.org/3/distutils/setupscript.html#installing-package-data
+    # data_files specify additional files that are not closely related to the
+    # source code of the package.
+    # https://docs.python.org/3/distutils/setupscript.html#installing-additional-files
+    package_data={'core': ['static/*.css', 'static/*.js', 'static/*.html', 'templates/*.html']},
+    zip_safe=False
 )
--- sok_project/SimpleDataVisualizer/simpleDataVisualizer/simple_visualizer.py
+++
@@ -1,2 +0,0 @@
-class SimpleVisualizer():
-    pass
