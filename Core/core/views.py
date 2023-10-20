eb9377af6101b6875e7c90a99fbc4c8f19f0e272
Bogdan <bogdanjanosevic@gmail.com>
2023-10-20 15:28:53 +0200

    #6 - Basic js for choosing a file, still not working

--- Core/core/views.py
+++ Core/core/views.py
@@ -13,7 +13,7 @@ def parse_and_visualize(request):
     parser = apps.get_app_config('core').pluginXml
     pluginVisualization = apps.get_app_config('core').pluginVisualization
 
-    parsed_data = parser[0].parse("../XMLFiles/universities.xml")
+    parsed_data = parser[0].parse("../XMLFiles/langualges.xml")
     print(parsed_data)
     parser[0].print_node(parsed_data)
 
@@ -22,6 +22,7 @@ def parse_and_visualize(request):
 
     visualization_html = pluginVisualization[0].visualize(nodes, links)
     visualization_html = mark_safe(visualization_html)
+    print(visualization_html)
 
     return render(request, 'index.html', {'visualization_html': visualization_html, 'nodes': nodes, 'links': links})
 
